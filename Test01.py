922a1f2c39846b41f40861d17f3a812a4a34b8c9
egbea123 <egbea123@hotmail.com>
2021-07-05 18:08:28 -0400

    add subtraction method

---
+++ Test01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+def main ():
+	num1 = 10.5
+	num2 = 6.3
+    # Subtract two numbers
+	sum = float(num1) - float(num2)
+	# Display the result
+	print('The sum of {0} and {1} is {2}',num1, num2, sum )
